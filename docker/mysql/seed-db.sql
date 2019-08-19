ecf997d3d36e1f85706721ca416413366dd10397
Michal Malecki <malek83@tlen.pl>
2019-08-19 18:15:35 +0200

    DB seed

--- docker/mysql/seed-db.sql
+++ docker/mysql/seed-db.sql
@@ -8,7 +8,16 @@ CREATE TABLE `node` (
                         `credits_right` int(11) NOT NULL,
                         `lft` int(11),
                         `rgt` int(11),
-                        PRIMARY KEY (`id`),
-                        FOREIGN KEY (lft) REFERENCES node(id),
-                        FOREIGN KEY (rgt) REFERENCES node(id)
+                        `direction` ENUM('L', 'R') NULL,
+                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+set names utf8;
+
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Aranżer', RAND(), RAND(), '1', '14', NULL);
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Artena', RAND(), RAND(), '2', '7', 'L');
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Szal', RAND(), RAND(), '3', '4', 'L');
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Arteria', RAND(), RAND(), '5', '6', 'R');
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Hebab', RAND(), RAND(), '8', '13', 'R');
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Navara', RAND(), RAND(), '9', '10', 'L');
+INSERT INTO `node` (`id`, `username`, `credits_left`, `credits_right`, `lft`, `rgt`, `direction`) VALUES (NULL, 'Bajgala', RAND(), RAND(), '11', '12', 'R');
